4d3976cf884d12539c4129734a3e1548bec1caa6
casa <casa@DESKTOP-GLEF2JS>
2016-05-23 21:27:16 -0300

    nuevo proyecto

--- accform/accProductoAgregar.php
+++ accform/accProductoAgregar.php
@@ -1,11 +1,9 @@
 <?php
 include("../librerias.php");
 session_start();
-$oPro=new Producto($nid=NULL,$snom=NULL,$ntot=NULL,$nano=NULL);
 
-$valor=$_POST['valor'];
-$snom=$_POST['nombre'];
-$anio=$_POST['anio'];
+
+
 
 //var_dump($_POST);
 
@@ -16,10 +14,14 @@ if (!isset($_SESSION["oUsuario"])){
 	document.location.href="index.php";
 </script>
 <?php
-
-//$oPro=$_SESSION["oPro"];
-//var_dump($oPro);
-if($oPro->Ingresar($snom,$valor,$anio)) echo "Producto ingresado"; else echo "ERROR";
 }
+$valor=$_POST['valor'];
+$snom=$_POST['nombre'];
+$anio=$_POST['anio'];
+
+$oPro=new Producto($nid=NULL,$snom=NULL,$ntot=NULL,$nano=NULL);
+
+if($oPro->Ingresar($snom,$valor,$anio)) echo "Producto ingresado"; else echo "ERROR";
+
 
 ?>
